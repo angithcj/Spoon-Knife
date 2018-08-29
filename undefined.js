4b5e123484c78545189e90f4e15ab4564780e599
angithcj <42056550+angithcj@users.noreply.github.com>
2018-08-29 08:34:49 +0530

    Add files via upload

---
+++ undefined.js
@@ -0,0 +1,13 @@
+
+var a;
+console.log(a);// prints undefined
+/*
+if var a is not defined prints error.else prints undefined
+undefined is a value assigned to uninitialized variables.
+*/
+if(a === undefined){
+  console.log('a is undefined');
+}
+else{
+  console.log('a is defined');
+}
